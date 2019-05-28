1cee8412a31b8572bb35a5be39ff34e668a04a7c
YOUSEF <yousef.alsatouf94@gmail.com>
2019-05-29 00:30:31 +0200

    created this react project this project is number 03

--- project-03/src/App.js
+++ project-03/src/App.js
@@ -1,34 +1,9 @@
-import React, { Component } from 'react';
-import ArticlesTable from './ArticlesTable';
-import $ from 'jquery';
-import './App.css';
+import React, {Component} from 'react';
+import ArticlesTable from 'ArticlesTable';
+import CommandsTable from 'CommandsTable';
 
 class App extends Component
 {
-    componentDidMount()
-    {
-        $.ajax({
-            url: 'http://127.0.0.1:8080/ReactFramework/project-03-react/public_html/articles.php',
-            type: 'GET',
-            dataType: 'json',
-            statusCode: {
-                404: f_404,
-                501: f_501
-            },
-            success: ajax_success,
-            error: ajax_error,
-            timeout: 2000
-        })
-    }
 
-    render()
-      {
-        return(
-            <div className="container">
-              <ArticlesTable />
-            </div>
-        )
-      }
 }
-
 export default App;
